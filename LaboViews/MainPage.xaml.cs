53821b5a8e452063f32417b51380d3d7c94b40fe
Peter Hauben <peter.hauben@student.pxl.be>
2025-10-01 19:40:36 +0200

    Werkend Labo3_Views

--- LaboViews/MainPage.xaml.cs
+++ LaboViews/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LaboViews
+﻿using Microsoft.Maui.Controls;
+
+namespace LaboViews
 {
     public partial class MainPage : ContentPage
     {
@@ -6,6 +8,45 @@
         {
             InitializeComponent();
         }
+
+        private void OnTapGestureRecognizerTapped(object sender, TappedEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                if (border.BackgroundColor == Colors.Black)
+                {
+                    border.BackgroundColor = Colors.DarkSlateGray;
+                }
+                else
+                {
+                    border.BackgroundColor = Colors.Black;
+                }
+            }
+        }
+
+        private async void OnTapGestureRecognizerTapped_AnimateY(object sender, TappedEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                await border.RotateYTo(360, 2000);
+                
+                border.RotationY = 0; // Terug op 0 zetten,
+                                        // anders blijt y op 360° staan
+                                        // en de rotatie wordt niet meer uitgevoerd
+            }
+        }
+
+        private async void OnTapGestureRecognizerTapped_AnimateX(object sender, TappedEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                await border.RotateXTo(360, 1000);
+                border.RotationX = 0; // Terug op 0 zetten,
+                                      // anders blijt y op 360° staan
+                                      // en de rotatie wordt niet meer uitgevoerd
+            }
+        }
+
     }
 
 }
